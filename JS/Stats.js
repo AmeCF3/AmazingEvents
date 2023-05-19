ee0d781bce7cc5a94f86e8cc00e7e6714bba35ca
AmeCF3 <aajcf3@gmail.com>
2023-05-18 23:08:44 -0300

    integración de la API

--- JS/Stats.js
+++ JS/Stats.js
@@ -1,17 +1,176 @@
 var stats = document.getElementById("stats")
 
-//Separar por categorias 
-
-function initStats() {
+async function initStats() {
     console.log(eventos)
 
+    // Obtenemos las categorias unicas
     var categorias = []
 
-    var unique = eventos.map(eventos => eventos.category) // separar eventos
-    const deleteRepeat = new Set(unique)
+    var unique = eventos.map(eventos => eventos.category)
+    const quitoRepetidas = new Set(unique)
     categorias = [...quitoRepetidas]
 
     console.log(categorias)
-    
-    var porCategorias = []
+
+    //Agrupamos los eventos por categorias
+    var porCategoria = []
+
+    categorias.forEach(categoria => {
+        porCategoria.push(
+            {
+                categoria: categoria,
+                data: eventos.filter(eventos => eventos.category === categoria)
+            }
+        )
+    })
+    console.log(porCategoria)
+    // Obtenemos datos de ingresos y asistencias o estimaciones por categoria
+
+    var ingresoYassitencia = []
+
+    porCategoria.map(datos => {
+        ingresoYassitencia.push({
+            categoria: datos.categoria,
+            ingreso: datos.data.map(item => item.assistance ? item.assistance * item.price : 0),
+            estimacionIngreso: datos.data.map(item => item.estimate ? item.estimate * item.price : 0),
+            asistencia: datos.data.map(item => item.assistance ? item.assistance : 0),
+            estimacionAsistencia: datos.data.map(item => item.estimate ? item.estimate : 0),
+            capacidad: datos.data.map(item => item.capacity ? item.capacity : 0)
+        })
+    })
+    //console.log(ingresoYassitencia)
+
+    // Sumo los datos generados en arrays en el paso anterior
+    ingresoYassitencia.forEach(categoria => {
+
+        //Calculo capacidades y asistencias para poder obtener los porcentajes necesarios 
+
+        let totalAsistencia = 0
+        let totalAsistenciaEstimada = 0
+        let totalCapacidadPasados = 0
+        let totalCapacidadFuturos = 0
+
+        for (var i = 0; i < categoria.ingreso.length; i++) {
+
+            //POr la estructura de datos si posee ingreso es un ecento pasado y mediante esta verificacion obtengo datos de asistencia y capcidad
+            if (categoria.ingreso[i] > 0) {
+                totalCapacidadPasados += categoria.capacidad[i]
+                totalAsistencia += categoria.asistencia[i]
+                categoria.totalCapacidadPasados = totalCapacidadPasados
+                categoria.totalAsistencia = totalAsistencia
+                // si no posee ingresos posee estimaciones y se trata de eventos futuros y mediante esto obtengo datos de estimaciones 
+            } else {
+                totalCapacidadFuturos += categoria.capacidad[i]
+                totalAsistenciaEstimada += categoria.estimacionAsistencia[i]
+                categoria.totalCapacidadFuturos = totalCapacidadFuturos
+                categoria.totalAsistenciaEstimada = totalAsistenciaEstimada
+            }
+        }
+        categoria.porcentajeDeAsistencia = "%" + ((totalAsistencia * 100) / totalCapacidadPasados).toFixed(2)
+        categoria.porcentajeDeEstimacion = "%" + ((totalAsistenciaEstimada * 100) / totalCapacidadFuturos).toFixed(2)
+
+        //Calculo el total de ingresos (PASADOS)
+        let totalIngreso = 0
+        categoria.ingreso.map(ingresos => totalIngreso += ingresos)
+        categoria.ingresos = totalIngreso
+
+        //Calculo el total de estimacion de Ingresos (FUTUROS)
+        let totalIngresoEstimado = 0
+        categoria.estimacionIngreso.map(ingresosEstimados => totalIngresoEstimado += ingresosEstimados)
+        categoria.estimacionIngresos = totalIngresoEstimado
+
+    })
+
+    console.log(ingresoYassitencia)
+    //Separo los eventos pasados que son los que poseen asistencia
+    let eventosPasados = []
+    let eventosFuturos = []
+    await eventos.filter(eventos => {
+        if (eventos.assistance) {
+            eventosPasados.push(eventos)
+        } else { eventosFuturos.push(eventos) }
+    })
+    //le agregop a cada evento una propiedad calculando el porcentaje de asistencia en funcion de la capacidad del evento
+
+    eventosPasados.map(eventos => {
+        eventos.porcentajeAsistencia = eventos.assistance * 100 / eventos.capacity
+    })
+
+    let asistenciaEventos = []
+    eventosPasados.filter(eventos => { asistenciaEventos.push(eventos.porcentajeAsistencia) })
+    // //Busco el mayor valor y filtro por este el array de eventos
+    let mayor = Math.max(...asistenciaEventos)
+    let eventoMayorAsistencia = eventos.filter(eventos => eventos.porcentajeAsistencia === mayor)
+
+    // Busco el menor valor y filtro por este el array de eventos pasados
+    let menor = Math.min(...asistenciaEventos)
+    let eventoMenorAsistencia = eventos.filter(eventos => eventos.porcentajeAsistencia === menor)
+
+    // Busco el evento con mayor capacidad por ordenamiento descendente
+    let mayorCapacidad = eventos.sort((a, b) => { return b.capacity - a.capacity })
+
+
+    //GENERO DATOS EN TABLA DE EVENTOS CON MAYOR Y MENOR ASISTENCIA Y CAPACIDAD
+    var rowMayoresYmenores = document.getElementById("mayoresymenores")
+    rowMayoresYmenores.innerHTML = ""
+    var tdMayorAsistencia = document.createElement("td")
+    var tdMenorAsistencia = document.createElement("td")
+    var tdMayorCapacidad = document.createElement("td")
+
+    rowMayoresYmenores.append(tdMayorAsistencia)
+    tdMayorAsistencia.append(eventoMayorAsistencia[0].name + " %" + eventoMayorAsistencia[0].porcentajeAsistencia.toFixed(2))
+
+    rowMayoresYmenores.append(tdMenorAsistencia)
+    tdMenorAsistencia.append(eventoMenorAsistencia[0].name + " %" + eventoMenorAsistencia[0].porcentajeAsistencia.toFixed(2))
+
+    rowMayoresYmenores.append(tdMayorCapacidad)
+    tdMayorCapacidad.append(mayorCapacidad[0].name + "(" + mayorCapacidad[0].category + ")")
+
+    //GENERAR LOS DATOS DE CATEGIRIAS DE EVENTOS FUTUROS
+
+    var tablaFuturos = document.getElementById("statsFuturos")
+
+    ordenarFuturos = []
+    ordenarFuturos.push(...ingresoYassitencia.sort((a, b) => {
+        return b.estimacionIngresos - a.estimacionIngresos
+    }))
+
+    ordenarFuturos.map(eventos => {
+        if (eventos.estimacionIngresos > 0) {
+            tablaFuturos.innerHTML +=
+                `
+        <tr>
+              <td>${eventos.categoria}</td>
+              <td>$${eventos.estimacionIngresos}</td>
+              <td>${eventos.porcentajeDeEstimacion}</td>
+            </tr>       
+        `
+        }
+    })
+
+
+    //GENERAR LOS DATOS DE CATEGIRIAS DE EVENTOS Pasados
+
+    var tablaPasados = document.getElementById("statsPasados")
+
+    let ordenarPasados = []
+    ordenarPasados.push(...ingresoYassitencia.sort((a, b) => {
+        return b.ingresos - a.ingresos
+    }))
+
+    ordenarPasados.map(evento => {
+        if (evento.ingresos > 0) {
+            tablaPasados.innerHTML +=
+                `
+     <tr>
+           <td>${evento.categoria}</td>
+           <td>$${evento.ingresos}</td>
+           <td>${evento.porcentajeDeAsistencia}</td>
+         </tr>       
+     `
+        }
+    })
+
 }
+
+
--- JS/details.js
+++ JS/details.js
@@ -1,41 +1,49 @@
-const eventos = dataAmazing.eventos
-console.log(location.search)
+let eventos
 var id = location.search.split("?id=").filter(Number)
-
 var selectId = id[0]
 console.log(selectId)
+const eventoDetalle = []
 
+var detalle = "";
+
+async function getData() {
+    let datosApi
+    await fetch("https://amd-amazingevents-api.onrender.com/api/eventos")
+        .then(response => response.json())
+        .then(json => datosApi = json)
+
+    eventos = datosApi.eventos
+    displayDetails()
+}
+getData()
 //Con los pasos anteriores sacamos el dato de un parametro,
 // se le hizo un split y filter para separar y filtrar solo el número
 
-const eventoDetalle = []
 
-for (var i = 0; i < eventos.length; i++) {
-    if (eventos[i].id == selectId) {
-        eventoDetalle.push(eventos[i])
+function displayDetails() {
+    for (var i = 0; i < eventos.length; i++) {
+        var asis_esti = eventos[i].assistance ? "Assistance" : "Estimate"
+        if (eventos[i].id == selectId) {
+            detalle += `
+        <div class="containerDetails">
+            <div class="container_img_details">
+                <img class="image_details" src="${eventos[i].image}" alt="Evento">
+            </div>
+            <div class="containter_description_details">
+                <div class="item_title_detils">
+                    <h2>${eventos[i].name}</h2>
+                </div>
+                <div class="item_description_details">
+                    <p class= "firs_detail" > ${eventos[i].description}</p>
+                </div>
+                <div class="more_details">
+                    <p>Precio: $${eventos[i].price}</p>
+                    <p>Fecha: ${eventos[i].date}</p>
+                </div>
+              </div>
+        </div>`
+            eventoDetalle.push(eventos[i])
+        }
     }
+    document.getElementById("prueba").innerHTML = detalle;
 }
-
-console.log(eventoDetalle[0])
-
-var texto = document.getElementById("prueba")
-
-texto.innerHTML =
-    `<div class="containerDetails">
-        <div class="container_img_details">
-            <img class="image_details" src="../Images/${eventoDetalle[0].image}" alt="Evento">
-        </div>
-        <div class="containter_description_details">
-            <div class="item_title_detils">
-                <h2>${eventoDetalle[0].name}</h2>
-            </div>
-            <div class="item_description_details">
-                <p class= "firs_detail" > ${eventoDetalle[0].description}</p>
-            </div>
-            <div class="more_details">
-                <p>Precio: $${eventoDetalle[0].price}</p>
-                <p>Fecha: ${eventoDetalle[0].date}</p>
-            </div>
-            
-        </div>
-    </div>`
